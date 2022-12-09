163874056a39fd83c2659cc4c936b69ac5dc6c60
Ayo Tosan Samuel <46663234+Stosan@users.noreply.github.com>
2022-12-09 02:37:41 +0100

    Update ETL_script.py

--- ETL_script.py
+++ ETL_script.py
@@ -1,36 +1,35 @@
 import requests
 import psycopg2
 
-# endpoint for getting user information
-ENDPOINT = "http://example.com/api/user"
+# endpoint for getting radio station information
+ENDPOINT = "http://example.com/api/station"
 
 # PostgreSQL database details
 DB_NAME = "mydatabase"
 DB_USER = "postgres"
 DB_PASSWORD = "password"
 
-# function to get user information from endpoint
-def get_user_info(endpoint):
+# function to get radio station information from endpoint
+def get_station_info(endpoint):
     response = requests.get(endpoint)
     if response.status_code == 200:
-        user_data = response.json()
-        return (user_data["name"], user_data["age"], user_data["gender"])
+        station_data = response.json()
+        return (station_data["station_name"], station_data["station_freq"], station_data["station_state"], station_data["station_uuid"])
     else:
         return None
 
-# function to seed PostgreSQL database with user information
-def seed_database(user_info):
+# function to seed PostgreSQL database with radio station information
+def seed_database(station_info):
     # connect to PostgreSQL database
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
 
-    # insert user information into database
-    cursor.execute("INSERT INTO radiostation_catalogues (station_name, station_freq, station_state, station_uuid) VALUES (%s, %s, %s, %s) RETURNING id", user_info)
-    catalogue_id = cursor.fetchone()[0]
+    # insert radio station information into database
+    cursor.execute("INSERT INTO radiostation_catalogues (station_name, station_freq, station_state, station_url,station_uuid) VALUES (%s, %s, %s, %s)", station_info)
 
-    # insert streams for user into database
-    for stream in user_data["streams"]:
-        cursor.execute("INSERT INTO radiostation_streams (catalogue_id, url) VALUES (%s, %s)", (catalogue_id, stream["url"]))
+    # insert streams for radio station into database
+    for stream in station_data["streams"]:
+        cursor.execute("INSERT INTO radiostation_streams (station_uuid, is_analyzed, audio_uid, audio_name) VALUES (%s, %s, %s, %s, %s)", (station_data["station_uuid"], stream["is_analyzed"], stream["audio_uid"], stream["audio_name"], stream["url"]))
 
     conn.commit()
 
@@ -38,8 +37,8 @@ def seed_database(user_info):
     cursor.close()
     conn.close()
 
-# continuously seed database with user information from endpoint
+# continuously seed database with radio station information from endpoint
 while True:
-    user_info = get_user_info(ENDPOINT)
-    if user_info:
-        seed_database(user_info)
+    station_info = get_station_info(ENDPOINT)
+    if station_info:
+        seed_database(station_info)
